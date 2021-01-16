43a732ea4c78f38f0ebc424acb3a40cd4a8eed24
Mikael Eliasson <mikael@bokio.se>
2021-01-16 08:06:35 +0100

    Initial model (version: Parent)

---
+++ EFCoreSnapshotCorruptionDemo/Blogpost.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EFCoreSnapshotCorruptionDemo
+{
+    public class Blogpost
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ EFCoreSnapshotCorruptionDemo/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSnapshotCorruptionDemo
+{
+    public class Context : DbContext
+    {
+        public DbSet<Blogpost> Blogposts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\;Initial Catalog=ef-corruption-demo;Integrated Security=True");
+            base.OnConfiguring(optionsBuilder);
+
+        }
+    }
+}
