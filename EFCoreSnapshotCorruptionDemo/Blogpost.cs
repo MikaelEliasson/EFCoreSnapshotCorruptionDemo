00d39de50eb8b77711243fcabe4b558996751d05
Mikael Eliasson <mikael@bokio.se>
2021-01-16 08:27:14 +0100

    Add Authors, corrupted migration (version:E)

--- EFCoreSnapshotCorruptionDemo/Blogpost.cs
+++ EFCoreSnapshotCorruptionDemo/Blogpost.cs
@@ -10,6 +10,6 @@ namespace EFCoreSnapshotCorruptionDemo
         public DateTime PublishDate { get; set; }
         public string Text { get; set; }
 
-        public string Author { get; set; }
+        public string Authors { get; set; }
     }
 }
---
+++ EFCoreSnapshotCorruptionDemo/Migrations/20210116072020_AddAuthors.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreSnapshotCorruptionDemo.Migrations
+{
+    public partial class AddAuthors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Authors",
+                table: "Blogposts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Blogposts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Authors",
+                table: "Blogposts");
+
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Blogposts");
+        }
+    }
+}
